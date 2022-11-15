fd849faf1d4a807b7aa6b4affccef3d4480aa9c4
Ichikyuu <ichikyuuofficial@gmail.com>
2022-11-15 13:14:58 +0700

    edit something

--- index.js
+++ index.js
@@ -1,131 +1,62 @@
-const Jimp = require('jimp');
-const rs = require('readline-sync');
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// random number with pattern xx-xxx-xxxxx
-function randomNumber() {
-    let pattern = "xx - xxx - xxxxx";
-    let random = pattern.replace(/x/g, function (c) {
-        let r = Math.random() * 10 | 0;
-        return r;
-    });
-    return random;
-}
-
-function randomBirthday() {
-
-    var day = Math.floor(Math.random() * 30) + 1;
-
-    var months = {
-        1: 'January',
-        2: 'February',
-        3: 'March',
-        4: 'April',
-        5: 'May',
-        6: 'June',
-        7: 'July',
-        8: 'August',
-        9: 'September',
-        10: 'October',
-        11: 'November',
-        12: 'December'
-    }
-
-    var month = Math.floor(Math.random() * 12) + 1;
-
-    // years from 1990 to 2003
-    var year = Math.floor(Math.random() * 13) + 1990;
-
-    return day + ' ' + months[month] + ' ' + year;
-
-}
-
-function randomDepartement() {
-
-    var departements = {
-        1: 'Biology',
-        2: 'Accounting',
-        3: 'Art & Design',
-        4: 'Information Technology',
-        5: 'Teacher Education',
-        6: 'Computer Science',
-        7: 'Administration',
-        8: 'Engineering',
-        9: 'Psychology',
-        10: 'Economics and Finance',
-        11: 'Educational Leadership',
-        12: 'Mathematics',
-        13: 'Political Science',
-        14: 'General Business',
-        15: 'Marketing and Management',
-        16: 'Chemistry and Biochemistry',
-        17: 'English and Modern Languages',
-        18: 'Information Systems Analysis',
-        19: 'M.B.A.',
-        20: 'Earth and Space Sciences',
-    }
-
-    var departement = Math.floor(Math.random() * 12) + 1;
-
-    return departements[departement];
-
-
-}
-
-const functionGenerateImage = async (name, img_profile) => {
-
-    try {
-
-        let nama = name;
-        let picture = await Jimp.read(img_profile);
-
-        
-
-        Jimp.read('./template/template.png', (err, image) => {
-
-            // Here is the code to add the image to the template
-            picture.resize(230, 280);
-            image.composite(picture, 60, 280);
-
-            if (err) throw err;
-
-            let textImage = new Jimp(1000,1000, 0x0, (err, textImage) => {  
-                //((0x0 = 0 = rgba(0, 0, 0, 0)) = transparent)
-                if (err) throw err;
-            })
-
-            // Add name
-            Jimp.loadFont(Jimp.FONT_SANS_32_BLACK).then(function (font) {
-
-                // textImage.color([{apply:'xor', params:['#46146b']}]);
-
-                textImage.print(font, 340, 300, `Name              :    ${nama}`);
-                image.composite(textImage, 0, 0);
-
-                textImage.print(font, 340, 360, `ID                    :    ${randomNumber()}`);
-                image.composite(textImage, 0, 0);
-
-                textImage.print(font, 340, 420, `DOB                :    ${randomBirthday()}`);
-                image.composite(textImage, 0, 0);
-
-                textImage.print(font, 340, 480, `Department     :    ${randomDepartement()}`);
-                image.composite(textImage, 0, 0);
-
-                image.write(`./results/${nama.replace(/ /g, '_')}.png`);
-
-            })
-
-        });
-
-        
-        
-    } catch (error) {
-        throw error;
-    }
-
-};
+const TimpaText = require('./src/index');
+const bot = new TimpaText();
 
 (async () => {
 
+    Head();
+
+    const questions_template = [
+        {
+            type: 'list',
+            name: 'template',
+            message: 'Choose template ?',
+            choices: fs.readdirSync(bot.dirTemplate)
+        },
+    ];
+    const answers_template = await inquirer.prompt(questions_template);
+
+    const questions_filename = [
+        {
+            type: 'list',
+            name: 'filename',
+            message: 'Choose filename ?',
+            choices: fs.readdirSync(bot.dirFilename)
+        }
+    ]
+    const answers_filename = await inquirer.prompt(questions_filename);
+
+    const questions_name = [
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Enter name ?'
+        }
+    ]
+    const answers_name = await inquirer.prompt(questions_name);
+
+    switch (answers_template.template) {
+        case 'IndianaTemplate.png':
+            await bot.IndianaTemplate(answers_name.name, answers_filename.filename, answers_template.template);
+            console.log({ success: true, path: `./results/${answers_name.name.replace(/ /g, '_')}.png` });
+            break;
+
+        case 'UMSTemplate.png':
+            await bot.UMSTemplate(answers_name.name, answers_filename.filename, answers_template.template);
+            console.log({ success: true, path: `./results/${answers_name.name.replace(/ /g, '_')}.png` });
+            break;
+
+        default:
+            break;
+
+    }
+
+
+})();
+
+function Head() {
     console.log(`
 ███████╗ █████╗ ██╗  ██╗███████╗██╗██████╗ 
 ██╔════╝██╔══██╗██║ ██╔╝██╔════╝██║██╔══██╗
@@ -135,23 +66,10 @@ const functionGenerateImage = async (name, img_profile) => {
 ╚═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝╚═════╝ 
 
 Note :
-- Save your image inside "img" folder
-- Input name manually
-- ID , DOB , and Department will be generated automatically
-- Result will be saved inside "results" folder
+
+1. Save your image inside "./image/pictures" folder ( .png or .jpg or .jpeg only )
+2. Input name manually
+3. ID , DOB , and Department will be generated automatically
+4. Result will be saved inside "./results" folder
     `)
-
-    var name = rs.question('Name : ');
-    var img = rs.question('Filename : ');
-    if (!img.includes('.png' || '.jpg' || '.jpeg')) {
-        console.log('Make sure your image is in .png or .jpg or .jpeg format');
-        return;
-    } else {
-
-        await functionGenerateImage(name, `./img/${img}`).then(() => {
-            console.log('Done !');
-        });
-
-    }
-
-})();
+}
